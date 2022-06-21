cde39f57df8d8d199e21e5f8f7c73273680b4c2d
Svetlozar Stoykov <deucyy@gmail.com>
2022-06-21 23:25:14 +0300

    Added various methods to activityStore to implement state management Implemented methods from activityStore for code cleanup Added Prettier config

--- client/src/app/layout/NavBar.tsx
+++ client/src/app/layout/NavBar.tsx
@@ -1,22 +1,29 @@
-import { Button, Container, Menu} from "semantic-ui-react";
+import { Button, Container, Menu } from "semantic-ui-react";
+import { useStore } from "../stores/store";
 
-interface Props{
-    openForm: (id?: number) => void;
-}
+export default function NavBar() {
+    const { activityStore } = useStore();
 
-export default function NavBar(props: Props){
-    return(
-        <Menu inverted fixed='top'>
+    return (
+        <Menu inverted fixed="top">
             <Container>
-                <Menu.Item header name='Reactivities'>
-                    <img src='/assets/logo.png' alt='logo' style={{marginRight: '10px'}}/>
+                <Menu.Item header name="Reactivities">
+                    <img
+                        src="/assets/logo.png"
+                        alt="logo"
+                        style={{ marginRight: "10px" }}
+                    />
                     Reactivities
                 </Menu.Item>
-                <Menu.Item name="Activities"/>
+                <Menu.Item name="Activities" />
                 <Menu.Item>
-                    <Button onClick = {() => props.openForm()} positive content='Create Activity'/>
+                    <Button
+                        onClick={() => activityStore.openForm()}
+                        positive
+                        content="Create Activity"
+                    />
                 </Menu.Item>
             </Container>
         </Menu>
-    )
+    );
 }
