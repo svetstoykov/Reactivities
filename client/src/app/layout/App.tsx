c981cfbe1c2eccff05da823d7f9f599d82aebba7
Svetlozar Stoykov <deucyy@gmail.com>
2022-07-19 08:55:59 +0300

    Added NotFound.tsx page and routing

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -4,19 +4,25 @@ import { Container } from "semantic-ui-react";
 import NavBar from "./NavBar";
 import { observer } from "mobx-react-lite";
 import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
-import { Route, useLocation } from "react-router-dom";
+import { Route, Switch, useLocation } from "react-router-dom";
 import HomePage from "../../features/home/HomePage";
 import ActivityForm from "../../features/activities/form/ActivityForm";
 import ActivityDetails from "../../features/activities/details/ActivityDetails";
-import {ToastContainer, Flip} from "react-toastify"
+import { ToastContainer, Flip } from "react-toastify";
 import TestErrors from "../../features/errors/TestErrors";
+import NotFound from "../../features/errors/NotFound";
 
 function App() {
     const location = useLocation();
 
     return (
         <>
-            <ToastContainer hideProgressBar theme="colored" transition={Flip} newestOnTop/>
+            <ToastContainer
+                hideProgressBar
+                theme="colored"
+                transition={Flip}
+                newestOnTop
+            />
             <Route exact path="/" component={HomePage} />
             <Route
                 path="/(.+)"
@@ -24,24 +30,24 @@ function App() {
                     <Fragment>
                         <NavBar />
                         <Container style={{ marginTop: "7em" }}>
-                            <Route
-                                path="/activities/:id"
-                                component={ActivityDetails}
-                            />
-                            <Route
-                                exact
-                                path="/activities"
-                                component={ActivityDashboard}
-                            />
-                            <Route
-                                key={location.key}
-                                path={["/createActivity", "/manage/:id"]}
-                                component={ActivityForm}
-                            />
-                            <Route
-                                path="/errors"
-                                component={TestErrors}
-                            />
+                            <Switch>
+                                <Route
+                                    path="/activities/:id"
+                                    component={ActivityDetails}
+                                />
+                                <Route
+                                    exact
+                                    path="/activities"
+                                    component={ActivityDashboard}
+                                />
+                                <Route
+                                    key={location.key}
+                                    path={["/createActivity", "/manage/:id"]}
+                                    component={ActivityForm}
+                                />
+                                <Route path="/errors" component={TestErrors} />
+                                <Route component={NotFound} />
+                            </Switch>
                         </Container>
                     </Fragment>
                 )}
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -7,13 +7,16 @@ import 'react-toastify/dist/ReactToastify.css';
 import App from "./app/layout/App";
 import reportWebVitals from "./reportWebVitals";
 import { store, StoreContext } from "./app/stores/store";
-import { BrowserRouter } from "react-router-dom";
+import { Router } from "react-router-dom";
+import {createBrowserHistory} from "history"
+
+export const history = createBrowserHistory()
 
 ReactDOM.render(
     <StoreContext.Provider value={store}>
-        <BrowserRouter>
+        <Router history={history}>
             <App />
-        </BrowserRouter>
+        </Router>
     </StoreContext.Provider>,
     document.getElementById("root")
 );
