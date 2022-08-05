7f64f07a809ab923ca9de98e6646b98fc522e63e
svetstoykov <deucyy@gmail.com>
2022-08-05 19:15:55 +0300

    Fixed typo in PicturesDataService

---
+++ Reactivities.Server/Persistence/Pictures/PicturesDataService.cs
@@ -0,0 +1,12 @@
+﻿using Application.Pictures;
+using Domain.Profiles;
+using Persistence.Common.DataServices;
+
+namespace Persistence.Pictures;
+
+public class PicturesDataService : EntityDataService<Picture>, IPicturesDataService
+{
+    public PicturesDataService(DataContext dataContext) : base(dataContext)
+    {
+    }
+}
